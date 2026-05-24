ca2184eb6bc95d814d65a6e947f59bf203ee6084
Fadilah <smailefuturelife@gmail.com>
2026-05-25 01:46:28 +0700

    Delete js directory

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  document.querySelector(".sidebar").classList.toggle("active");
-}
-
-async function loadComponent(id, file) {
-  const res = await fetch(file);
-  const html = await res.text();
-  document.getElementById(id).innerHTML = html;
-}
